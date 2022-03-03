0b102fdc887bd4917eb6bd490f04893f96917ea4
Halina Balys <hbalys@gmail.com>
2022-03-03 11:25:23 +0100

    init commit

---
+++ api/baseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  BASE_URL: "https://volunteers-manager.firebaseio.com/volunteers.json",
+  DELETE_URL: "https://volunteers-manager.firebaseio.com/volunteers/",
+  LOGIN_URL:
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAQkN9ckqMBR-Gh0vXe85lrMOKdRyquAcU",
+  SIGNUP_URL:
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQkN9ckqMBR-Gh0vXe85lrMOKdRyquAcU",
+  UPDATE_URL:
+    "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAQkN9ckqMBR-Gh0vXe85lrMOKdRyquAcU"
+};
